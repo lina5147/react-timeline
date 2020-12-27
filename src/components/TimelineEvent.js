af22e7681a0847040a014603aee1c48bb8bc071c
Lina Do <l.do5147@gmail.com>
2020-12-27 15:48:11 -0800

    implemented the TimelineEvent component - takes three props, matched the class names to css file

--- src/components/TimelineEvent.js
+++ src/components/TimelineEvent.js
@@ -2,9 +2,23 @@ import React from 'react';
 import './TimelineEvent.css';
 import Timestamp from './Timestamp';
 
-const TimelineEvent = () => {
+const TimelineEvent = (props) => {
   
-  return;
+  return (
+    <div className="timeline-event">
+      <div className="event-person">
+       {props.person}
+      </div>
+      <div className="event-status">
+       {props.status}
+      </div>
+      <div className="event-time">
+        <Timestamp time={props.timeStamp} />
+      </div>
+    </div>
+
+
+  )
 }
 
 export default TimelineEvent;
