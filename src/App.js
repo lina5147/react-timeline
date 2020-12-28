8331df2cd452a821e104f68418e2c25df637cab1
Lina Do <l.do5147@gmail.com>
2020-12-27 16:45:04 -0800

    added 's to title of app

--- src/App.js
+++ src/App.js
@@ -4,14 +4,13 @@ import timelineData from './data/timeline.json';
 import Timeline from './components/Timeline';
 
 function App() {
-
   const events = timelineData.events;
 
   // Customize the code below
   return (
     <div className="App">
       <header className="App-header">
-        <h1 className="App-title">{timelineData.person} social media feed</h1>
+        <h1 className="App-title">{timelineData.person}'s social media feed</h1>
       </header>
       <main className="App-main">
         <Timeline events={events} />
